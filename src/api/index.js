8d1b0d1d8775f8f7ad5a81ead8e2bd60cb233105
wangyong <wangyong@cmsr.chinamobile.com>
2022-10-30 22:45:52 +0800

    店铺组件

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,11 @@ export function getStatistics1() {
   return request.get("/admin/statistics1");
 }
 
+export function getStatistics2() {
+  return request.get("/admin/statistics2");
+}
+
+
 export function getStatistics3(type) {
   return request.get("/admin/statistics3?type=" + type);
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -51,11 +51,14 @@
   </div>
   <Navs></Navs>
 
-  <el-row :gutter="20">
+  <el-row :gutter="20" class="mt-5">
     <el-col :span="12" :offset="0">
       <IndexChart />
     </el-col>
-    <el-col :span="12" :offset="0"></el-col>
+    <el-col :span="12" :offset="0">
+      <IndexCard title="店铺及商品提示" tip="店铺及商品提示" :btns="goods" class="mb-3" />
+      <IndexCard title="交易提示" tip="需要立即处理的交易订单" :btns="order" />
+    </el-col>
   </el-row>
 
 </template>
@@ -64,13 +67,21 @@ import { ref } from 'vue'
 import CountTo from '../components/CountTo.vue';
 import Navs from '../components/Navs.vue'
 import IndexChart from "../components/Echart.vue";
-import { getStatistics1 } from "@/api/index"
+import IndexCard from "../components/Card.vue";
+import { getStatistics1, getStatistics2 } from "@/api/index"
 const panels = ref([])
+const loading = ref(true)
 getStatistics1().then(res => {
   panels.value = res.panels
   loading.value = false
 })
 
-const loading = ref(true)
+
+const goods = ref([])
+const order = ref([])
+getStatistics2().then(res => {
+  goods.value = res.goods
+  order.value = res.order
+})
 
 </script>
