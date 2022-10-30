a0d60714ad725809c96b07993d97fad81f7d6737
wangyong <wangyong@cmsr.chinamobile.com>
2022-10-30 17:35:53 +0800

    keep-alive和transtion全局动画

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -9,7 +9,13 @@
       </el-aside>
       <el-main>
         <f-tag-list />
-        <router-view></router-view>
+        <router-view v-slot="{ Component }">
+          <transition name="fade">
+            <keep-alive :max="10">
+              <component :is="Component"></component>
+            </keep-alive>
+          </transition>
+        </router-view>
       </el-main>
     </el-container>
   </el-container>
@@ -21,8 +27,40 @@ import FTagList from './components/FTagList.vue';
 </script>
 
 <style>
-.el-aside{
+.el-aside {
   /* 消除闪一下的效果 */
   transition: all 0.2s;
+}
+
+.fade-enter-from {
+  opacity: 0;
+  -webkit-transform: translate3d(0, -100%, 0);
+  transform: translate3d(0, -100%, 0);
+}
+
+.fade-enter-to {
+  opacity: 1;
+  -webkit-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+
+
+.fade-leave-to {
+  opacity: 0;
+  -webkit-transform: translate3d(0, 100%, 0);
+  transform: translate3d(0, 100%, 0);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 10s;
+}
+
+.fade-enter-active {
+  transition-delay: 10s;
 }
 </style>
